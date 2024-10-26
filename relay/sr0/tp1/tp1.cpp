fa48dabd7b5ea3efcd842bcb7b464013d550b913
Cristian Amarie <ca@rocknowradio.ro>
2024-10-26 13:25:47 +0300

    - [wip] cmake-based openssl

---
+++ relay/sr0/tp1/tp1.cpp
@@ -0,0 +1,41 @@
+﻿#include "tp1.h"
+
+class logger {
+public:
+	static void vlog(const char* level, const char* fmt, va_list ap) {
+		char buf[512] = {0};
+		vsnprintf(buf, sizeof(buf), fmt, ap);
+		fprintf(stdout, "%s %s\n", level, buf);
+	}
+	static void __cdecl log(const char* level, const char* fmt, ...) {
+		va_list ap;
+		va_start(ap, fmt);
+		vlog(level, fmt, ap);
+		va_end(ap);
+	}
+};
+#define INFO(fmt, ...)  logger::log("INFO ", fmt, __VA_ARGS__)
+#define ERROR(fmt, ...) logger::log("ERROR", fmt, __VA_ARGS__)
+
+class RelayConnectionStarter {
+private:
+	int relayPort = -1;
+	std::string relayIP;
+public:
+	RelayConnectionStarter(const std::string& relayIP, int relayPort) 
+		: relayIP(relayIP)
+		, relayPort(relayPort) {
+	}
+	void run() {
+		//	create SSL socket
+	}
+};
+
+int main() {
+	INFO("Hello CMake");
+
+	RelayConnectionStarter relay("193.29.58.141", 19002);
+	relay.run();
+	
+	return 0;
+}
---
+++ relay/sr0/tp1/tp1.h
@@ -0,0 +1,8 @@
+﻿// tp1.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <cstdarg>
+#include <cstdio>
+#include <string>
